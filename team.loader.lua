da8d709b86ae67a97a889702084ed0040cbead1f
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-18 19:20:59 -0300

    Update team.loader.lua

--- team.loader.lua
+++ team.loader.lua
@@ -1,18 +1,19 @@
 print(getgenv().team)
-
-local team = tostring(getgenv().team)
-
-if team == "" or team == " " then
-    team = "Marines"
-    getgenv().team = team
-end
-
-if team == "Pirates" or team == "Marines" then
-    local args = {
-        [1] = "SetTeam",
-        [2] = team
-    }
-    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+if getgenv().team == "" or getgenv().team == " " then
+getgenv().team = "Marines"
 else
-    print("Pirates or Marines! you put: " .. team)
+print("")
 end
+
+if tostring(getgenv().team) == "Pirates" or tostring(getgenv().team) == "Marines" then
+local args = {
+[1] = "SetTeam",
+[2] = tostring(getgenv().team)
+}
+
+game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+
+else
+print("Pirates or Marines! you put: ".. getgenv().team )
+end
+
